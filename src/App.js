8bbb6b602f38fe23e755a05f0ebfa577ff6a39e5
KhurramMunir95 <khurrammunir9522@gmail.com>
2021-02-09 20:53:54 +0500

    app.js file updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 
 function App() {
   const app = 'This is a React App';
+  const url = 'https://www.google.com/';
   return (
     <div className="App">
       <h1>{app}</h1>
+      <a href={url}>Click this link</a>
     </div>
-  
   );
 }
 
